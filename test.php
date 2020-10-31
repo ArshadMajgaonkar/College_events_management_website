ad751f5a95a2a80b62ef6c4410a4df251e304908
Arshad Majgaonkar <arshadmaj608@gmail.com>
2020-10-31 14:42:19 +0530

    initial commit

---
+++ test.php
@@ -0,0 +1,28 @@
+
+<?php
+
+define('DB_HOST','localhost');
+define('DB_NAME','college_eventsDB');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','root');
+
+$connection=mysqli_connect(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME) or die("cannot connect");
+
+$sql = "SELECT * FROM `admin_login` ";
+                         $result = mysqli_query($connection, $sql);
+
+
+                         while ($row = mysqli_fetch_array($result)) {
+                            echo '  
+                            <tr>  
+                                 <td>' . $row["AEN"] . '</td>  
+                                 <td>' . $row["student_id"] . '</td>  
+                                 <td>' . $row["branch"] . '</td>  
+                                 <td>' . $row["year"] . '</td>  
+                                 <td>' . $row["name"] . '</td></tr>';
+
+                         }
+
+                            
+
+?>
